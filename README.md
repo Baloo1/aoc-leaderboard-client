f7f5d581be3c649665b2e88e321d48fcb03c24a5
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-05 20:16:57 +0100

    Add first project version

---
+++ README.md
@@ -0,0 +1,40 @@
+# Advent of Code Leaderboard Client
+
+This script takes the JSON from Advent of Code leaderboards and 
+formats it into a leaderboard with absolute times instead of stars. 
+It then outputs it into a md file. 
+
+Get your input from AoC. Paste it into input.json. Run with `npm start`. It will choose default input from input.json and expects it to exist. For any
+other input file start with `npm start INPUTFILENAME`. In the future this may be improved.
+
+You have to manually get the input from a leaderboards API section. 
+
+## Requirements
+
+- Node 15
+- AoC leaderboard
+
+## Example output
+
+The repo includes example input in input.json. Just replace it with your own data. 
+It includes sample output in Leaderboard 1.md.
+
+# Leaderboard 1  
+*Day 1*  
+
+Time | Id | Name  
+--- | --- | ---  
+06:10:54| 1 | Person 2  
+06:12:25| 2 | Person 2  
+06:46:56| 1 | Person 1  
+06:50:18| 2 | Person 1  
+13:22:08| 1 | Person 6  
+13:33:13| 2 | Person 6  
+14:30:25| 1 | Person 8  
+14:30:39| 2 | Person 8  
+16:06:52| 1 | Person 5  
+16:31:23| 1 | Person 4  
+16:39:34| 2 | Person 4  
+19:32:35| 2 | Person 5  
+20:58:27| 1 | Person 3  
+21:01:15| 2 | Person 3    
---
+++ leaderboard.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const inputFile = process.argv[2] || "input.json";
+
+let input = {}; // You can paste your AoC leaderboard JSON here or import from file
+input = JSON.parse(fs.readFileSync(inputFile));
+
+const formattedTime = (date) => {
+    const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    const minutes = "0" + date.getMinutes();
+    const seconds = "0" + date.getSeconds();
+    return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+}
+
+const formatEntry = (id, s, name) => {
+    const time = formattedTime(new Date(+s.get_star_ts * 1000));
+    return `${time}| ${id} | ${name}  `
+}
+
+let output = `# Leaderboard ${Object.values(input.owner_id)}  \n`;
+for (let i = 0; i < 26; i++) {
+
+    /*Credit to https://github.com/lindskogen/ for oneliner*/
+    const entries = Object.values(input.members).flatMap(m => Object.entries(m.completion_day_level[i] ?? {}).map(([id, s]) => formatEntry(id, s, m.name))).sort().join("\n")
+    if (entries) {
+        output +=
+            `*Day ${i}*  \n
+Time | Id | Name  
+--- | --- | ---  \n`;
+        output += entries;
+        output += `  \n`
+    }
+}
+
+fs.writeFileSync(`Leaderboard ${input.owner_id}.md`, output)
+console.log(`Done! Printed to file: Leaderboard ${input.owner_id}.md`)
