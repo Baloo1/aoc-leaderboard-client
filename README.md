c5aef8bd061889c7c013e61a17738d66ec22a57b
Johannes Mattsson <karlmattsson96@gmail.com>
2020-12-13 00:30:45 +0100

    Add automatic board fetching

--- README.md
+++ README.md
@@ -2,13 +2,10 @@
 
 This script takes the JSON from Advent of Code leaderboards and 
 formats it into a leaderboard with absolute times instead of stars. 
-It then outputs it into a md file. 
+It then outputs it into a ~~md~~ file. Markdown may be fixed or not.
 
-Get your input from AoC. Paste it into input.json. Run with `npm start`. It will choose default input from input.json and expects it to exist. For any
-other input file start with `npm start INPUTFILENAME`. In the future this may be improved.
-
-You have to manually get the input from a leaderboards API section. Can be found from [AoC website](https://adventofcode.com/2020/leaderboard/private/view/YOURIDHERE.json) 
-by replacing YOURIDHERE in the URL.
+It Gets your input from AoC. Just create a file `myCookie.txt` and paste your cookie  from AoC in it. Run with `npm start`. 
+Getting cookies programmatically is ~~WIP~~ hard. Right now you can find your cookie in your preferred browser.
 
 ## Requirements
 
@@ -17,25 +14,24 @@ by replacing YOURIDHERE in the URL.
 
 ## Example output
 
-The repo includes example input in input.json. Just replace it with your own data. 
-It includes sample output in Leaderboard 1.md.
+The repo includes example output in Leaderboard 1.md.
 
 # Leaderboard 1  
 *Day 1*  
 
-Time | Id | Name  
---- | --- | ---  
-06:10:54| 1 | Person 2  
-06:12:25| 2 | Person 2  
-06:46:56| 1 | Person 1  
-06:50:18| 2 | Person 1  
-13:22:08| 1 | Person 6  
-13:33:13| 2 | Person 6  
-14:30:25| 1 | Person 8  
-14:30:39| 2 | Person 8  
-16:06:52| 1 | Person 5  
-16:31:23| 1 | Person 4  
-16:39:34| 2 | Person 4  
-19:32:35| 2 | Person 5  
-20:58:27| 1 | Person 3  
-21:01:15| 2 | Person 3    
+   Time | ★ | Pos | Name
+   --- | :---: | :---:  | ---
+06:10:54   | 1 |   1 | Person 2
+06:12:25   | 2 |   1 | Person 2
+06:46:56   | 1 |   2 | Person 1
+06:50:18   | 2 |   2 | Person 1
+13:22:08   | 1 |   3 | Person 6
+13:33:13   | 2 |   3 | Person 6
+16:06:52   | 1 |   4 | Person 8
+16:31:23   | 1 |   5 | Person 8
+16:39:34   | 2 |   4 | Person 5
+19:32:35   | 2 |   5 | Person 4
+20:58:27   | 1 |   6 | Person 4
+21:01:15   | 2 |   6 | Person 5
+14:30:25+1 | 1 |   7 | Person 3
+14:30:39+1 | 2 |   7 | Person 3
--- leaderboard.js
+++ leaderboard.js
@@ -1,8 +1,37 @@
 const fs = require("fs");
-const inputFile = process.argv[2] || "input.json";
+const fetch = require('node-fetch');
+const readlines = require('readline')
+const cookie = fs.readFileSync('myCookie.txt');
+let leaderboardId = '';
 
-let input; // You can paste your AoC leaderboard JSON here or import from file
-input = JSON.parse(fs.readFileSync(inputFile));
+const getLeaderboard = () => {
+    const readline = readlines.createInterface(process.stdin, process.stdout);
+    
+    readline.setPrompt("Input leaderboard id. Wont sanitize so just numbers\n")
+    readline.prompt()
+    return new Promise(resolve => {
+        readline.on('line', id => {
+            leaderboardId = id.trim();
+            readline.close();
+        })
+
+        readline.on('close', () => {
+            resolve();
+        })
+    });
+}
+
+const getData = async (cookie) => {
+    const leaderboardUrl = `https://adventofcode.com/2020/leaderboard/private/view/${leaderboardId}.json`
+
+    const response = await fetch(leaderboardUrl,
+        {
+            headers: {
+                Cookie: cookie
+            }
+        });
+    return await response.json();
+}
 
 const formattedTime = (date) => {
     const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
@@ -20,40 +49,49 @@ const mapData = (starIdx, star, name, challengeDay) => {
     return {ts, time, starIdx, name};
 };
 
-const formatEntry = ({time, starIdx, name}, position) =>
-    `${time} | ${starIdx} | ${position} | ${name}`;
+const main = async () => {
+    await getLeaderboard();
+    input = await getData(cookie)
 
-let output = `# Leaderboard ${input.owner_id}  \n`;
-for (let day = 0; day < 26; day++) {
-    const positions = {
-        1: 1,
-        2: 1,
-    };
+    const formatEntry = ({time, starIdx, name}, position) =>
+        `${time} | ${starIdx} | ${position} | ${name}`;
 
-    /* Credit to https://github.com/lindskogen/ for "magic script" */
-    const entries = Object.values(input.members)
-        .filter((member) => member.completion_day_level[day] != null)
-        .flatMap((member) =>
-            Object.entries(member.completion_day_level[day]).map(([starIdx, star]) =>
-                mapData(starIdx, star, member.name, day)
+    let output = `# Leaderboard ${input.owner_id}  \n`;
+    for (let day = 0; day < 26; day++) {
+        const positions = {
+            1: 1,
+            2: 1,
+        };
+
+        /* Credit to https://github.com/lindskogen/ for "magic script" */
+        const entries = Object.values(input.members)
+            .filter((member) => member.completion_day_level[day] != null)
+            .flatMap((member) =>
+                Object.entries(member.completion_day_level[day]).map(([starIdx, star]) =>
+                    mapData(starIdx, star, member.name, day)
+                )
             )
-        )
-        .sort((left, right) => left.ts - right.ts)
-        .map((data) => {
-            const position = ("   " + positions[data.starIdx]).slice(-3);
-            positions[data.starIdx] += 1;
-            return formatEntry(data, position);
-        })
-        .join("\n");
+            .sort((left, right) => left.ts - right.ts)
+            .map((data) => {
+                const position = ("   " + positions[data.starIdx]).slice(-3);
+                positions[data.starIdx] += 1;
+                return formatEntry(data, position);
+            })
+            .join("\n");
 
-    if (entries) {
-        output += `*Day ${day}*\n      Time | ★ | Pos | Name\n`;
-        output += entries;
-        output += `  \n\n`;
+        if (entries) {
+            output += `*Day ${day}*\n      Time | ★ | Pos | Name\n`;
+            output += entries;
+            output += `  \n\n`;
+        }
     }
+    if (!fs.existsSync("./leaderboards")) {
+        fs.mkdirSync("./leaderboards")
+    }
+    fs.writeFileSync(`./leaderboards/Leaderboard ${input.owner_id}.md`, output);
+    console.log(`Done! Printed to file: Leaderboard ${input.owner_id}.md`);
+    process.exit(0);
 }
-if (!fs.existsSync("./leaderboards")) {
-    fs.mkdirSync("./leaderboards")
-}
-fs.writeFileSync(`./leaderboards/Leaderboard ${input.owner_id}.md`, output);
-console.log(`Done! Printed to file: Leaderboard ${input.owner_id}.md`);
+
+main();
+
